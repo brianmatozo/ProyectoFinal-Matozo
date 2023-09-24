4878b7775002dba982761fff7f015cbffd1eb136
brianmatozo <brianmatozo@gmail.com>
2023-09-24 17:28:09 -0300

    ciclos y calculos

--- js/main.js
+++ js/main.js
@@ -1,32 +1,36 @@
-let valoritem = 1000
+
+
 function ivacalc(){ 
     let ivacal = prompt("quieres calcular el iva?")
     if (ivacal == "SI") {
-        valoritem*1.21
-        console.log("aca el valor con iva")
+        valoritemiva = valoritem*1.21
+        console.log("aca el valor con iva", valoritemiva)
     }
     else if (ivacal == "NO"){
-        console.log("aca el valor sin iva");
+        console.log("aca el valor sin iva", valoritem);
     }
     else{
         prompt("profavor ingresa SI o NO")
     }
     }
 
-let compra = prompt("Que item quiere comprar (1, 2, 3, ESC)");
+let compra = parseInt(prompt("Que item quiere comprar (1, 2, 3, ESC)"));
 while(compra != "ESC"){
     switch (compra){
         case "1":
+        valoritem = 6995.5
         ivacalc();
         break;
         case "2":
+        valoritem = 10495
         ivacalc();
         break;
         case "3":
+        valoritem = 13994.5
         ivacalc();
         break;
         default:
-            aler("default")
+            alert("default")
         break;
     }
     compra = prompt("Que item quiere comprar (1, 2, 3, ESC)");
