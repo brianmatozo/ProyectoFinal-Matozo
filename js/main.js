965b30a8f4ecb17f8ede4cc6e609f334e98769a5
brianmatozo <brianmatozo@gmail.com>
2023-10-11 04:48:03 -0300

    suma de total no estaba completa

--- js/main.js
+++ js/main.js
@@ -1,45 +1,3 @@
-// function ivacalc(){ 
-//     let ivacal = prompt("quieres calcular el iva?")
-//     if (ivacal == "SI") {
-//         valoritemiva = valoritem*1.21
-//         console.log("aca el valor con iva", valoritemiva)
-//     }
-//     else if (ivacal == "NO"){
-//         console.log("aca el valor sin iva", valoritem);
-//     }
-//     else{
-//         prompt("profavor ingresa SI o NO")
-//     }
-//     }
-// let compra = parseInt(prompt("Que item quiere comprar (1, 2, 3, ESC)"));
-// while(compra != "ESC"){
-//     switch (compra){
-//         case "1":
-//         valoritem = 6995.5
-//         ivacalc();
-//         break;
-//         case "2":
-//         valoritem = 10495
-//         ivacalc();
-//         break;
-//         case "3":
-//         valoritem = 13994.5
-//         ivacalc();
-//         break;
-//         default:
-//             alert("default")
-//         break;
-//     }
-//     compra = prompt("Que item quiere comprar (1, 2, 3, ESC)");
-// }
-
-// Toggle the cart when the "Cart" button is clicked
-$(document).ready(function () {
-    $("#cart-toggle").click(function () {
-        $(".floating-cart").toggleClass("show");
-    });
-});
-
 document.addEventListener("DOMContentLoaded", function() {
     const comprarBtn = document.querySelectorAll(".btnorange");
     const totalElement = document.getElementById("total");
@@ -50,8 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
             event.preventDefault();
             const productName = button.getAttribute("data-name");
             const productValor = parseFloat(button.getAttribute("data-valor"));
-
-
 
             // crearitems
             const productInfoElement = document.createElement("ul");
